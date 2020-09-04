341e4e608f24bbdb154af514f776877a27501fd1
dorian <dorian@mino.rs>
2020-09-04 12:26:39 +0100

    standardise code against manual version

--- exp.js
+++ exp.js
@@ -3,23 +3,23 @@
 		/* set up */
 		////////////
 
-		var instructions_on = 0; // you can turn off (0) the first two instructions screens if you want to test (since the participant recording test takes a bit)
+		var instructions_on = 1; // you can turn off (0) the first two instructions screens if you want to test (since the participant recording test takes a bit)
 
 		var num_blocks = 2; // will repeat each block of stimuli this number of times (blocked together)
 		var num_tr_blocks = 1; // number of training blocks (same principle as num_blocks)
-		var window_height = window.innerHeight; // get the window height in pixels	
-       
+
         //////////////////////
         /* stimuli creation */
         //////////////////////
 
         var colours = ["red", "blue", "green"];
 
+		var window_height = window.innerHeight; // get the window height in pixels	
         var stim_height = { // stimulus height in pixels - width is auto (i.e. will maintain aspect ratio)
 			small: window_height*0.1,
 			medium: window_height*0.3,
 			large: window_height*0.5
-        } 
+		}
 
         // little stimulus factory we'll use later when constructing the trials
         // produces a complete stimulus object that can be indexed into by a trial variablec as a timeline variable.
@@ -57,7 +57,7 @@
         
             let printed_words = [...colours]
             // in this case, the print is the same as the colours (or based off the colours in the case of the false fonts)
-            // with different printed words, just replace 'doFalseFont' with prints and delete the doFalseFont line in the function
+            // with different printed words, just replace 'doFalseFont' with prints and replace the references to `final_print`
         
             colours.forEach(colour => {
                 // console.log('Ink colour: ' + colour)
@@ -88,11 +88,11 @@
 
 		var timeline = []; // initialise timeline
 		
-		//////////////////
-		/* instructions */
-		//////////////////	
+		//////////////////////
+		/* instruction bits */
+		//////////////////////
 
-		var instructions_on_start = {
+		var instructions_onstart = {
 			type: 'html-keyboard-response',
 			stimulus:"<p>In this experiment you'll see images on the screen and respond by speaking aloud.<br>You'll need to allow microphone access in your browser if you haven't already.<br>Be sure to 'remember the decision' so you don't get prompted every time.<br>There are four different task in this experiment.<br>Each one is slightly different, although all are similar.<br>At the start of each task, you'll get some instructions.<br>Then there will be a short 'training' period during which we'll tell you the correct answer after each trial.<br>Then you'll start the block properly and you won't get any feedback until the next block.<br><br>When ready, press any key continue.</p>"
 		}
@@ -110,13 +110,10 @@
 
 		/* push those to the timeline, if instructions are on */
 		if (instructions_on == 1) {
-			timeline.push(instructions_on_start);
+			timeline.push(instructions_onstart);
 			timeline.push(participant_test);
 		}
 
-		//////////////////////
-		/* instruction bits */
-		//////////////////////
 
 		/* report size instructions */
 		var size_instructions = {
@@ -177,7 +174,7 @@
 					type: 'html-keyboard-response',
 					stimulus: '<div style="font-size:60px;">+</div>',
 					choices: jsPsych.NO_KEYS,
-                    trial_duration: 300,
+					trial_duration: 300,
                     data: jsPsych.timelineVariable('add_data') // pull this in so we can access it in a subsequent trial
 				},
 				{ // training block, colour: shows correct response for colour -- stroop_task.timeline[1]
@@ -224,8 +221,8 @@
 			// 'repetitions:' would go here, but we will assign this more dynamically later
 		}
         console.log(stroop_task.timeline_variables);
-        
-        /* false font task */
+
+		/* false font task */
 		var false_font_task = {
 			timeline: [
 				{
@@ -273,11 +270,11 @@
 					recording_indicator: 2,
 					data: jsPsych.timelineVariable('add_data')
 				}
-            ],
+			],
             timeline_variables: stimListFactory(colours, true, Object.keys(stim_height)),
 			randomize_order: true,
 			// 'repetitions:' would go here, but we will assign this more dynamically later
-        }
+		}
         console.log(false_font_task.timeline_variables);
 
 		//////////////////////////////////////////////////////
@@ -298,10 +295,10 @@
 		////////////////////////
 
 		var stroop_colour_proc = [
-			colour_instructions,
-			pre_training,
+			colour_instructions, // precede stroop with colour instructions
+			pre_training, // pre task instructions
 			// now we spread (shallow copy) the block object, and add to the keys inside - we need to be careful here, because it will only shallow copy: editing too deep will permanently alter the block object
-			{...stroop_task, timeline: [stroop_task.timeline[0], stroop_task.timeline[1], colour_feedback], repetitions: num_tr_blocks},
+			{...stroop_task, timeline: [stroop_task.timeline[0], stroop_task.timeline[1], colour_feedback], repetitions: num_tr_blocks}, // append feedback to the stroop and add repetitions
 			pre_test,
 			// same again - spread the block object and add to the keys inside
 			{...stroop_task, timeline: [stroop_task.timeline[0], stroop_task.timeline[3]], repetitions: num_blocks},
